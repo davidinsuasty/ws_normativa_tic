749e7cbb1a56e52127dde3c490cc16c63d4de9f2
davidinsuasty <davidinsuasty@gmail.com>
2025-03-13 13:29:31 -0500

    fix error for decretos

--- Scrapping_TIC.py
+++ Scrapping_TIC.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 import time
 import sqlite3
+import re
 
 
 def create_db(conn):
@@ -96,58 +97,87 @@ if __name__ == "__main__":
                     driver.execute_script(f"window.open('{href}', '_blank');")
                     time.sleep(1)  # Esperar a que cargue el contenido
                     driver.switch_to.window(driver.window_handles[-1])
+                    ### modificar para el filtro de decretos
 
-                    years = driver.find_elements(By.CLASS_NAME, "opcion-year")
-                    prev_pag_2 = driver.current_window_handle
+                    if category == "Decretos":
+                        prev_pag_2 = driver.current_window_handle
+                        btn_years = driver.find_element(
+                            By.CLASS_NAME, "boton-selector-year"
+                        )
+                        btn_years.click()
+                        time.sleep(1)
+                        year_texts = re.findall(
+                            r"\b(19\d{2}|20\d{2})\b", btn_years.text
+                        )
+                    else:
+                        year_texts = ["", ""]
 
-                    for year in years:
-                        year_txt = year.find_element(
-                            By.CLASS_NAME, "titulo-opcion-year"
-                        ).text
-                        print(f"---> Año: {year_txt}")
-                        documentos = year.find_elements(By.CLASS_NAME, "opcion-nueva")
+                    #####
+                    for i in range(1, len(year_texts)):
+                        if category == "Decretos":
+                            selector = driver.find_element(
+                                By.XPATH,
+                                f"/html/body/div[1]/div/main/div/div/nav/form/div/div/div[1]/div/div[2]/div[{i}]",
+                            )
+                            driver.execute_script("arguments[0].click();", selector)
+                            time.sleep(1)
 
-                        for doc in documentos:
-                            extracted_data = {}
+                        years = driver.find_elements(By.CLASS_NAME, "opcion-year")
+                        if category != "Decretos":
+                            prev_pag_2 = driver.current_window_handle
 
-                            doc_title = doc.find_element(
-                                By.CLASS_NAME, "id-documento"
+                        for year in years:
+                            year_txt = year.find_element(
+                                By.CLASS_NAME, "titulo-opcion-year"
                             ).text
-                            print(f"----> Doc: {doc_title}")
-                            doc_desc = doc.find_element(
-                                By.CLASS_NAME, "descripcion-documento"
-                            ).text
-
-                            href = doc.find_element(By.TAG_NAME, "a").get_attribute(
-                                "href"
+                            print(f"---> Año: {year_txt}")
+                            documentos = year.find_elements(
+                                By.CLASS_NAME, "opcion-nueva"
                             )
-                            driver.execute_script(f"window.open('{href}', '_blank');")
-                            time.sleep(1)  # Esperar a que cargue el contenido
-                            driver.switch_to.window(driver.window_handles[-1])
-                            url = driver.current_url
 
-                            # Leer documento
-                            div_doc = driver.find_element(
-                                By.CLASS_NAME, "panel-documento"
-                            )
-                            text_elements = div_doc.find_elements(
-                                By.XPATH, ".//a | .//p"
-                            )
-                            text = [elem.text for elem in text_elements]
-                            all_text = "\n".join(text)
+                            for doc in documentos:
+                                extracted_data = {}
 
-                            extracted_data["id-doc"] = doc_title
-                            extracted_data["description"] = doc_desc
-                            extracted_data["year"] = year_txt
-                            extracted_data["entidad"] = name_entidad
-                            extracted_data["category"] = category
-                            extracted_data["url-doc"] = url
-                            extracted_data["text"] = all_text
+                                doc_title = doc.find_element(
+                                    By.CLASS_NAME, "id-documento"
+                                ).text
+                                print(f"----> Doc: {doc_title}")
+                                doc_desc = doc.find_element(
+                                    By.CLASS_NAME, "descripcion-documento"
+                                ).text
 
-                            save_on_db(cursor, extracted_data)
-                            # Cerrar la nueva pestaña y volver a la original (opcional)
-                            driver.close()
-                            driver.switch_to.window(prev_pag_2)
+                                href = doc.find_element(By.TAG_NAME, "a").get_attribute(
+                                    "href"
+                                )
+                                driver.execute_script(
+                                    f"window.open('{href}', '_blank');"
+                                )
+                                time.sleep(1)  # Esperar a que cargue el contenido
+                                driver.switch_to.window(driver.window_handles[-1])
+                                url = driver.current_url
+
+                                # Leer documento
+                                div_doc = driver.find_element(
+                                    By.CLASS_NAME, "panel-documento"
+                                )
+                                text_elements = div_doc.find_elements(
+                                    By.XPATH, ".//a | .//p"
+                                )
+                                text = [elem.text for elem in text_elements]
+                                all_text = "\n".join(text)
+
+                                extracted_data["id-doc"] = doc_title
+                                extracted_data["description"] = doc_desc
+                                extracted_data["year"] = year_txt
+                                extracted_data["entidad"] = name_entidad
+                                extracted_data["category"] = category
+                                extracted_data["url-doc"] = url
+                                extracted_data["text"] = all_text
+
+                                save_on_db(cursor, extracted_data)
+                                # Cerrar la nueva pestaña y volver a la original (opcional)
+                                driver.close()
+                                driver.switch_to.window(prev_pag_2)
 
                     driver.close()
                     driver.switch_to.window(prev_pag_1)
